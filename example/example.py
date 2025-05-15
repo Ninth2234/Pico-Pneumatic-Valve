d944d5ae96f215fb919de02303456722c3edeb76
Ninth2234 <85066094+Ninth2234@users.noreply.github.com>
2025-05-15 17:22:37 +0700

    first commit

---
+++ example/example.py
@@ -0,0 +1,13 @@
+rom machine import Pin
+from time import sleep
+
+pin_in1 = Pin(0,Pin.OUT)
+pin_in2 = Pin(1,Pin.OUT)
+
+pin_in2.high()
+
+while True:
+    pin_in1.high()
+    sleep(1)
+    pin_in1.low()
+    sleep(1)
